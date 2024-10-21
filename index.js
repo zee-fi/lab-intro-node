b7dd68d7c56192d26d941dcb0e0dc5545ca8e5ad
fiona-eh <fionahulse@mac.com>
2024-10-21 17:35:01 +0200

    completed lab

--- index.js
+++ index.js
@@ -1,17 +1,49 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items = this.items.sort((a,b) => a-b);
 
-  get(pos) {}
+  }
 
-  max() {}
+  get(pos) {
+    if (pos > this.items.length) {
+    throw new Error("OutOfBounds");
+    }
+    return this.items[pos];
+  }
 
-  min() {}
+  max() {
+    if(this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return this.items.slice(-1);
+  }
 
-  sum() {}
+  min() {
+    if(this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return this.items[0];
+  }
 
-  avg() {}
+  sum() {
+    return this.items.reduce((acc, cV) => acc + cV, 0);
+  }
+
+  avg() {
+    if(this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    let avg = this.items.reduce((acc, cV) => acc+cV, 0);
+    return avg / this.items.length;
+  }
 }
+
+const newSortedList = new SortedList();
 
 module.exports = SortedList;
